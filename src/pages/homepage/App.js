657b832c386dd9d7dc3e93c1e8056e676e4a8026
sirbruno <bruno.webjr@gmail.com>
2021-07-30 22:59:00 -0300

    ...

--- src/pages/homepage/App.js
+++ src/pages/homepage/App.js
@@ -18,11 +18,10 @@ function App() {
         <div>
           <h1>Hi, my name is Bruno Pereira, and i'm a Frontend Developer.</h1>
           <p>São Paulo, Brazil.</p>
-          <p>Skills: <b>React.js</b> | <b>Node.js</b></p>
+          <p>Skills: <b>React.js</b></p>
           <p>
             <a className={styles.socialIcon} href="https://github.com/sirbruno" target="_blank" rel="noreferrer"><i className="fab fa-github"></i></a>
             <a className={styles.socialIcon} href="https://www.linkedin.com/in/sirbruno" target="_blank" rel="noreferrer"><i className="fab fa-linkedin"></i></a>
-            <a className={styles.socialIcon} href="https://wa.me/5519971159735" target="_blank" rel="noreferrer"><i className="fab fa-whatsapp-square"></i></a>
           </p>
           {/* <Counter /> */}
         </div>
