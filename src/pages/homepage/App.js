2442a02340ecf1d9d1a632fdbf7986a5bea8bf10
sirbruno <bruno.webjr@gmail.com>
2021-08-13 22:38:02 -0300

    improving the design

--- src/pages/homepage/App.js
+++ src/pages/homepage/App.js
@@ -17,9 +17,9 @@ function App() {
         </nav>
         <header className={styles.AppHeader}>
           <div>
-            <h1>Hello! My name is Bruno, and I'm a Frontend Developer.</h1>
+            <h1 className={styles.title}>Hello! My name is Bruno, and I'm a Frontend Developer.</h1>
             <p className={styles.subTitle}>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.</p>
-            <p>
+            <p className={styles.socialMediaIcons}>
               <a className={styles.socialIcon} href="https://github.com/sirbruno" target="_blank" rel="noreferrer"><i className="fab fa-github"></i></a>
               <a className={styles.socialIcon} href="https://www.linkedin.com/in/sirbruno" target="_blank" rel="noreferrer"><i className="fab fa-linkedin"></i></a>
             </p>
