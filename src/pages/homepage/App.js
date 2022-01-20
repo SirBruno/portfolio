a1415c003ca4ae8f8765df95d3f07d8db010e50a
sirbruno <bruno.webjr@gmail.com>
2022-01-19 22:42:54 -0300

    Added some design elements.

--- src/pages/homepage/App.js
+++ src/pages/homepage/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 // import { Counter } from '../../features/counter/Counter';
 import styles from './App.module.css';
 import { Link, useLocation } from 'react-router-dom'
+import img from '../../assets/img/yoshiko-evanka-oI90dywodT8.jpg'
 
 function App() {
 
@@ -18,7 +19,7 @@ function App() {
           <a href="#contact">Contact</a>
         </nav>
         <header className={styles.AppHeader}>
-          <div>
+          <div style={{ position: 'relative', zIndex: '1' }}>
             <h1 className={styles.title}>Hello! My name is Bruno, and I'm a Front-end Developer.</h1>
             <p className={styles.subTitle}>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.</p>
             <p className={styles.socialMediaIcons}>
@@ -27,6 +28,9 @@ function App() {
             </p>
             {/* <Counter /> */}
           </div>
+            <div style={{position: 'absolute', left: '100px', bottom: '0', width: '700px', height: '750px'}}>
+              <img src={img} alt='illustrative' style={{width: '100%', overflow: 'auto', maxHeight: '100%', objectFit: 'cover', opacity: '.5'}} />
+            </div>
         </header>
       </div>
       <section id="aboutMe" className={styles.aboutMe}>
