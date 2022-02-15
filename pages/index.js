f7eff470820139a47a4d5c74c804aaf56b77305f
bruno-valtech <bruno.pereira@valtech.com>
2022-02-15 08:30:15 -0300

    Added title and subtitle.

--- pages/index.js
+++ pages/index.js
@@ -12,14 +12,8 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+        <h1 className={styles.title}>Bruno Pereira</h1>
+        <h2 className={styles.subtitle}>Web Developer</h2>
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
