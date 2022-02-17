addcb41f3c812dd171d95482b86ea5088bb2307a
sirbruno <bruno.webjr@gmail.com>
2022-02-16 22:27:52 -0300

    Added some more elements from the design.

--- pages/index.js
+++ pages/index.js
@@ -12,11 +12,14 @@ export default function Home() {
       </Head>
 
       <section className={styles.one}>
+        <span className={styles.nav}>-- navigation goes here --</span>
         <section className={styles.hero}>
           <main className={styles.main}>
             <h1 className={styles.title}>Bruno Pereira</h1>
             <h2 className={styles.subtitle}>Web Developer</h2>
             <p className={styles.p}>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.</p>
+            <br />
+            <p className={styles.p}>-- CTA goes here --</p>
           </main>
           <div className={styles.heroRight}>
             <p className={styles.p}>-- image goes here --</p>
