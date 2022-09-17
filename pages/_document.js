f2d6684056c8719a45a0b824609fc855ee91bc36
sirbruno <bruno.webjr@gmail.com>
2022-09-17 15:01:45 -0300

    Fixed favicon issue

--- pages/_document.js
+++ pages/_document.js
@@ -8,9 +8,8 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Fira+Sans:wght@100;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap" rel="stylesheet" />
-        <link rel="icon" type="image/png" sizes="32x32" href="../public/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="../public/favicon-16x16.png" />
-        <link rel="manifest" href="../public/site.webmanifest" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png" />
       </Head>
       <body>
         <Main />
