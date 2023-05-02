9a7d68fb676e0e7e8f141d294260402e1e062311
SirBruno <bruno.webjr@gmail.com>
2023-05-02 15:28:08 +0900

    added more animations

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import { useEffect } from 'react'
 import './App.css'
 import { gsap } from 'gsap'
+import heroIcon from './assets/heroIcon.png'
+import { ReactComponent as YourSvg } from './assets/hero-icon.svg'
 
 function App() {
 
-  const timeline = gsap.timeline();
-
+  
   useEffect(() => {
+    const timeline2 = gsap.timeline({repeat: -1});
+    timeline2.pause()
+    const timeline = gsap.timeline();
+
     if (typeof window !== 'undefined') {
       window.onload = () => {
-        timeline.from([".App h1", ".App .heroShape"], {
+        timeline.from([".App h1 span"], {
           y: 200,
-          stagger: 0.5,
-          opacity: 0
+          duration: 0.5,
+          opacity: 0,
+          ease: "power1.inOut",
+          stagger: 0.2
         })
 
         timeline.from([".nav__logo"], {
@@ -27,6 +34,52 @@ function App() {
           opacity: 0,
           duration: 0.2
         }, "<")
+
+        timeline.to([".nav__logo img"], {
+          rotation: 360,
+          x: 60,
+          duration: 1,
+          ease: "power4.out"
+        }, "<")
+
+        timeline.to([".nav__logo img"], {
+          rotation: -390,
+          x: -5,
+          duration: 3,
+          ease: "power1.inOut"
+        }, "-=0.5")
+
+        timeline.to([".nav__logo img"], {
+          rotation: -360,
+          x: 0,
+          duration: 1,
+          ease: "back.in"
+        })
+
+        // timeline.to([".nav__logo img"], {
+        //   rotation: 360,
+        //   duration: 5,
+        //   ease: 'none',
+        //   repeat: -1,
+        // })
+
+        timeline.call(() => timeline2.play())
+
+        timeline2.to(".App h1 span", {
+          filter: "blur(5px)",
+          duration: 1,
+          stagger: 0.2
+        })
+
+        timeline2.to(".App h1 span", {
+          filter: "blur(0px)",
+          duration: 1,
+          stagger: 0.2
+        }, "-=0.5")
+
+        timeline2.to(".App h1 span", {
+          delay: 5
+        })
       }
     }
   }, [])
@@ -34,14 +87,29 @@ function App() {
   return (
     <div className="App">
       <nav className="nav">
-        <span className="nav__logo">LOGO</span>
+        <span className="nav__logo">
+          {/* <YourSvg /> */}
+          <img src={heroIcon} alt="Hero icon" />
+        </span>
         <div className="nav__links">
-          <a>Link 1</a>
-          <a>Link 2</a>
-          <a>Link 3</a>
+          <a href="google.com">Link 1</a>
+          <a href="google.com">Link 2</a>
+          <a href="google.com">Link 3</a>
         </div>
       </nav>
-      <h1>Hello!</h1>
+      <h1>
+        <span>B</span>
+        <span>R</span>
+        <span>U</span>
+        <span>N</span>
+        <span>O</span>
+        <span> </span>
+        <span>S</span>
+        <span>I</span>
+        <span>L</span>
+        <span>V</span>
+        <span>A</span>
+      </h1>
     </div>
   );
 }
