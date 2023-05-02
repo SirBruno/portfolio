7a1e1cc9700d04f62b376f1e4379b7f2408ad9be
SirBruno <bruno.webjr@gmail.com>
2023-05-02 11:33:28 +0900

    added a navigation

--- src/App.js
+++ src/App.js
@@ -1,42 +1,47 @@
 import { useEffect } from 'react'
 import './App.css'
 import { gsap } from 'gsap'
-import heroShape from './assets/heroShape.svg'
 
 function App() {
+
+  const timeline = gsap.timeline();
 
   useEffect(() => {
     if (typeof window !== 'undefined') {
       window.onload = () => {
-        gsap.from([".App h1", ".App .heroShape"], {
+        timeline.from([".App h1", ".App .heroShape"], {
           y: 200,
           stagger: 0.5,
           opacity: 0
         })
+
+        timeline.from([".nav__logo"], {
+          x: -200,
+          duration: 0.2,
+          opacity: 0
+        })
+        
+        timeline.from([".nav__links a"], {
+          x: 200,
+          stagger: 0.1,
+          opacity: 0,
+          duration: 0.2
+        }, "<")
       }
-
-      document.querySelectorAll(".App")[0].addEventListener("mouseleave", () => gsap.to(".heroShape", {
-        x: 0,
-        y: 0,
-        transform: "none",
-        delay: 0.1
-      }));
-
-      document.querySelectorAll(".App")[0].addEventListener("mousemove", (e) => gsap.to(".heroShape", {
-        x: (e.pageX - window.innerWidth / 2) / 10,
-        y: (e.pageY - window.innerHeight / 2) / 10,
-        duration: 0.5,
-        rotation: (e.pageY - window.innerHeight / 2) / 50,
-        ease: "power3",
-      }));
     }
   }, [])
 
   return (
     <div className="App">
+      <nav className="nav">
+        <span className="nav__logo">LOGO</span>
+        <div className="nav__links">
+          <a>Link 1</a>
+          <a>Link 2</a>
+          <a>Link 3</a>
+        </div>
+      </nav>
       <h1>Hello!</h1>
-      {/* <span class="backgroundOverlay"></span> */}
-      <img src={heroShape} className="heroShape" />
     </div>
   );
 }
