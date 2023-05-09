0e7b13a00302df07b91a736fa41505107592357e
SirBruno <bruno.webjr@gmail.com>
2023-05-09 09:57:03 +0900

    removed some unused code.

--- src/App.js
+++ src/App.js
@@ -7,78 +7,25 @@ function App() {
 
   
   useEffect(() => {
-    const timeline2 = gsap.timeline({repeat: -1});
+
+    gsap.defaults({ duration: 1, stagger: 0.2 })
+
+    const timeline2 = gsap.timeline({repeat: -1})
     timeline2.pause()
-    const timeline = gsap.timeline();
+    const timeline = gsap.timeline()
 
     if (typeof window !== 'undefined') {
       window.onload = () => {
-        timeline.from([".App h1 span"], {
-          y: 200,
-          duration: 0.5,
-          opacity: 0,
-          ease: "power1.inOut",
-          stagger: 0.2
-        })
-
-        timeline.from([".nav__logo"], {
-          x: -200,
-          duration: 0.2,
-          opacity: 0
-        })
-        
-        timeline.from([".nav__links a"], {
-          x: 200,
-          stagger: 0.1,
-          opacity: 0,
-          duration: 0.2
-        }, "<")
-
-        timeline.to([".nav__logo img"], {
-          rotation: 360,
-          x: 60,
-          duration: 1,
-          ease: "power4.out"
-        }, "<")
-
-        timeline.to([".nav__logo img"], {
-          rotation: -390,
-          x: -5,
-          duration: 3,
-          ease: "power1.inOut"
-        }, "-=0.5")
-
-        timeline.to([".nav__logo img"], {
-          rotation: -360,
-          x: 0,
-          duration: 1,
-          ease: "back.in"
-        })
-
-        // timeline.to([".nav__logo img"], {
-        //   rotation: 360,
-        //   duration: 5,
-        //   ease: 'none',
-        //   repeat: -1,
-        // })
-
+        timeline.from([".App h1 span"], { y: 200, duration: 0.5, opacity: 0, ease: "power1.inOut" })
+        timeline.from([".nav__logo"], { x: -200, duration: 0.2, opacity: 0 })
+        timeline.from([".nav__links a"], { x: 200, stagger: 0.1, opacity: 0, duration: 0.2 }, "<")
+        timeline.to([".nav__logo img"], { rotation: 360, x: 60, ease: "power4.out" }, "<")
+        timeline.to([".nav__logo img"], { rotation: -390, x: -5, duration: 3, ease: "power1.inOut" }, "-=0.5")
+        timeline.to([".nav__logo img"], { rotation: -360, x: 0, ease: "back.in" })
         timeline.call(() => timeline2.play())
-
-        timeline2.to(".App h1 span", {
-          filter: "blur(5px)",
-          duration: 1,
-          stagger: 0.2
-        })
-
-        timeline2.to(".App h1 span", {
-          filter: "blur(0px)",
-          duration: 1,
-          stagger: 0.2
-        }, "-=0.5")
-
-        timeline2.to(".App h1 span", {
-          delay: 5
-        })
+        timeline2.to(".App h1 span", { filter: "blur(5px)" })
+        timeline2.to(".App h1 span", { filter: "blur(0px)" }, "-=0.5")
+        timeline2.to(".App h1 span", { delay: 5 })
       }
     }
   }, [])
