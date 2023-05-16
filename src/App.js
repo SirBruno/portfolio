dba85a9ae677dc239817f4dba475a512894962ef
SirBruno <bruno.webjr@gmail.com>
2023-05-16 09:54:59 +0900

    mobile issues

--- src/App.js
+++ src/App.js
@@ -27,6 +27,17 @@ function App() {
         timeline2.to(".App h1 span", { filter: "blur(0px)" }, "-=0.5")
         timeline2.to(".App h1 span", { delay: 5 })
       }
+    } else {
+      timeline.from([".App h1 span"], { y: 200, duration: 0.5, opacity: 0, ease: "power1.inOut" })
+      timeline.from([".nav__logo"], { x: -200, duration: 0.2, opacity: 0 })
+      timeline.from([".nav__links a"], { x: 200, stagger: 0.1, opacity: 0, duration: 0.2 }, "<")
+      timeline.to([".nav__logo img"], { rotation: 360, x: 60, ease: "power4.out" }, "<")
+      timeline.to([".nav__logo img"], { rotation: -390, x: -5, duration: 3, ease: "power1.inOut" }, "-=0.5")
+      timeline.to([".nav__logo img"], { rotation: -360, x: 0, ease: "back.in" })
+      timeline.call(() => timeline2.play())
+      timeline2.to(".App h1 span", { filter: "blur(5px)" })
+      timeline2.to(".App h1 span", { filter: "blur(0px)" }, "-=0.5")
+      timeline2.to(".App h1 span", { delay: 5 })
     }
   }, [])
 
