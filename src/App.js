371988850af3390f11e1bab1dd021bc3895ad047
SirBruno <bruno.webjr@gmail.com>
2023-05-18 12:20:10 +0900

    turned the hero into a component

--- src/App.js
+++ src/App.js
@@ -1,59 +1,14 @@
-import { useLayoutEffect, useRef } from 'react'
+import { useRef } from 'react'
 import './App.css'
-import { gsap } from 'gsap'
-import heroIcon from './assets/heroIcon.png'
+import Hero from './components/Hero'
 
 function App() {
 
   const app = useRef();
-  
-  useLayoutEffect(() => {
-    let ctx = gsap.context(() => {
-      gsap.defaults({ duration: 1, stagger: 0.2 })
-
-      const timeline2 = gsap.timeline({repeat: -1})
-      timeline2.pause()
-      const timeline = gsap.timeline()
-
-      timeline.from([".App h1 span"], { y: 200, duration: 0.5, opacity: 0, ease: "power1.inOut" })
-      timeline.from([".nav__logo"], { x: -200, duration: 0.2, opacity: 0 })
-      timeline.from([".nav__links a"], { x: 200, stagger: 0.1, opacity: 0, duration: 0.2 }, "<")
-      timeline.to([".nav__logo img"], { rotation: 360, x: 60, ease: "power4.out" }, "<")
-      timeline.to([".nav__logo img"], { rotation: -390, x: -5, duration: 3, ease: "power1.inOut" }, "-=0.5")
-      timeline.to([".nav__logo img"], { rotation: -360, x: 0, ease: "back.in" })
-      timeline.call(() => timeline2.play())
-      timeline2.to(".App h1 span", { filter: "blur(5px)" })
-      timeline2.to(".App h1 span", { filter: "blur(0px)" }, "-=0.5")
-      timeline2.to(".App h1 span", { delay: 5 })
-    }, app)
-
-    return () => ctx.revert()
-  }, [])
 
   return (
     <div ref={app} className="App">
-      <nav className="nav">
-        <span className="nav__logo">
-          <img src={heroIcon} alt="Hero icon" />
-        </span>
-        <div className="nav__links">
-          <a href="https://github.com/SirBruno" target='_blank' rel="noreferrer">GitHub</a>
-          <a href="https://linkedin.com/profile/sibruno" target='_blank' rel="noreferrer">LinkedIn</a>
-        </div>
-      </nav>
-      <h1>
-        <span>B</span>
-        <span>R</span>
-        <span>U</span>
-        <span>N</span>
-        <span>O</span>
-        <span> </span>
-        <span>S</span>
-        <span>I</span>
-        <span>L</span>
-        <span>V</span>
-        <span>A</span>
-      </h1>
+      <Hero app={app} />
     </div>
   );
 }
---
+++ src/components/Hero.js
@@ -0,0 +1,60 @@
+import { useLayoutEffect } from "react";
+import { gsap } from 'gsap'
+import heroIcon from '../assets/heroIcon.png'
+import './Hero.css'
+
+function Hero(props) {
+
+  useLayoutEffect(() => {
+    let ctx = gsap.context(() => {
+      gsap.defaults({ duration: 1, stagger: 0.2 })
+
+      const timeline2 = gsap.timeline({ repeat: -1 })
+      timeline2.pause()
+      const timeline = gsap.timeline()
+
+      timeline.from([".hero__title span"], { y: 200, duration: 0.5, opacity: 0, ease: "power1.inOut" })
+      timeline.from([".hero__logoContainer"], { x: -200, duration: 0.2, opacity: 0 })
+      timeline.from([".nav__link"], { x: 200, stagger: 0.1, opacity: 0, duration: 0.2 }, "<")
+      timeline.to([".hero__logo"], { rotation: 360, x: 60, ease: "power4.out" }, "<")
+      timeline.to([".hero__logo"], { rotation: -390, x: -5, duration: 3, ease: "power1.inOut" }, "-=0.5")
+      timeline.to([".hero__logo"], { rotation: -360, x: 0, ease: "back.in" })
+      timeline.call(() => timeline2.play())
+      timeline2.to(".hero__title span", { filter: "blur(5px)" })
+      timeline2.to(".hero__title span", { filter: "blur(0px)" }, "-=0.5")
+      timeline2.to(".hero__title span", { delay: 5 })
+    }, props.app)
+
+    return () => ctx.revert()
+  }, [props])
+
+  return (
+    <div className="hero">
+      <nav className="hero__nav">
+        <span className="hero__logoContainer">
+          <img className="hero__logo" src={heroIcon} alt="hero__icon" />
+        </span>
+        <div className="nav__linkContainer">
+          <a className="nav__link" href="https://github.com/SirBruno" target='_blank' rel="noreferrer">GitHub</a>
+          <a className="nav__link" href="https://linkedin.com/profile/sibruno" target='_blank' rel="noreferrer">LinkedIn</a>
+        </div>
+      </nav>
+      <h1 className="hero__title">
+        <span>B</span>
+        <span>R</span>
+        <span>U</span>
+        <span>N</span>
+        <span>O</span>
+        <span> </span>
+        <span>S</span>
+        <span>I</span>
+        <span>L</span>
+        <span>V</span>
+        <span>A</span>
+      </h1>
+    </div>
+  )
+
+}
+
+export default Hero
