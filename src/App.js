139e90be0af8786835e2dbfbaf5ab71f7f91983f
SirBruno <bruno.webjr@gmail.com>
2023-05-20 10:23:00 +0900

    added a new section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css'
-import Hero from './components/Hero'
+import Hero from './components/Hero/Hero'
 
 function App() {
 
   return (
     <div className="App">
       <Hero />
+      <section>
+        <h2>HTML & CSS</h2>
+        <p>I spent my entire teenagehood studying only HTML & CSS, not only because that's the only thing I needed to customize my website, but also because I was afraid of JavaScript back then.</p>
+      </section>
     </div>
   );
 }
