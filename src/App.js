9a6bd0b2351b9f4663ea0e4a91701fc2d23e22da
SirBruno <bruno.webjr@gmail.com>
2023-06-29 22:39:50 +0900

    replaced one of the sections

--- src/App.js
+++ src/App.js
@@ -9,7 +9,10 @@ function App() {
   return (
     <div className="App">
       <Hero gsap={gsap} ScrollTrigger={ScrollTrigger} />
-      <HtmlCss gsap={gsap} ScrollTrigger={ScrollTrigger} />
+      <Section title={"About me"}>
+        Hi, my name is Bruno, and I'm a web developer from Brazil, currently living in Japan. I started learning HTML & CSS when I was 15, then learned JavaScript when I was in college. After I came to Japan, I decided to go one step further and learn how to do complex animations with GSAP, though I'm still working on it.
+      </Section>
+      {/* <HtmlCss gsap={gsap} ScrollTrigger={ScrollTrigger} /> */}
       <Section theme={"dark"} title={"JavaScript"}>
         I began learning JS in 2017 when I started college. Back then I didn't know anything other than HTML & CSS, but I knew I wanted to specialize in web development, so JavaScript was the language I chose for most of my college projects.
       </Section>
