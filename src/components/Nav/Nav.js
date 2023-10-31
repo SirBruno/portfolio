a14e60e32d091ed1343f7e697f62a72136c9ae2d
SirBruno <bruno.webjr@gmail.com>
2023-10-31 17:39:04 +0900

    redesign.

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Nav.css'
+
+function Nav() {
+  return(
+    <nav className="nav">
+      <span className="nav__text">Everything 100% off.</span>
+    </nav>
+  )
+}
+
+export default Nav
