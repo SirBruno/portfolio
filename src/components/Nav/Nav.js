a38c5732461da511b13eb99e834db0e96cde31d1
SirBruno <bruno.webjr@gmail.com>
2023-10-31 18:05:28 +0900

    added discounted price.

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -4,7 +4,7 @@ import './Nav.css'
 function Nav() {
   return(
     <nav className="nav">
-      <span className="nav__text">Everything 100% off.</span>
+      <span className="nav__text">Everything 50% off.</span>
     </nav>
   )
 }
