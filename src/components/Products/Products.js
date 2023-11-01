a60bdbdcb1898f2a235bd553a36f09f7bf76f24f
SirBruno <bruno.webjr@gmail.com>
2023-11-01 11:59:48 +0900

    added mock api.

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,66 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Product.css'
 
 function Product() {
+
+  const [products, setProducts] = useState(null);
+
+  useEffect(() => {
+    const getData = async () => {
+      const url = "https://fakestoreapi.com/products";
+
+      try {
+        const resp = await fetch(url);
+        const data = await resp.json();
+        // Do anything you need to do to
+        // data before this call:
+        setProducts(data);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+
+    getData();
+  }, []);
+
+  console.log("products")
+  console.log(products)
+
   return (
     <div className="Products">
       <div className="Products__Container">
         <h3>Products</h3>
         <div className="Products__Grid">
-          <div className="Products__Card">
-            <span className="Products__Image"></span>
-            <span className="Products__CardTitle">Lorem ipsum dolor sit amet</span>
-            <span className="Products__CardDescription">men's clothing</span>
-            <div className="Products__PriceBox">
-              <span className="Products__Price">$49.97</span>
-              <span className="Products__Price Discounted">$99.95</span>
-            </div>
-          </div>
-          <div className="Products__Card">
-            <span className="Products__Image"></span>
-            <span className="Products__CardTitle">Lorem ipsum dolor sit amet</span>
-            <span className="Products__CardDescription">men's clothing</span>
-            <div className="Products__PriceBox">
-              <span className="Products__Price">$49.97</span>
-              <span className="Products__Price Discounted">$99.95</span>
-            </div>
-          </div>
-          <div className="Products__Card">
-            <span className="Products__Image"></span>
-            <span className="Products__CardTitle">Lorem ipsum dolor sit amet</span>
-            <span className="Products__CardDescription">men's clothing</span>
-            <div className="Products__PriceBox">
-              <span className="Products__Price">$49.97</span>
-              <span className="Products__Price Discounted">$99.95</span>
-            </div>
-          </div>
-          <div className="Products__Card">
-            <span className="Products__Image"></span>
-            <span className="Products__CardTitle">Lorem ipsum dolor sit amet</span>
-            <span className="Products__CardDescription">men's clothing</span>
-            <div className="Products__PriceBox">
-              <span className="Products__Price">$49.97</span>
-              <span className="Products__Price Discounted">$99.95</span>
-            </div>
-          </div>
-          <div className="Products__Card">
-            <span className="Products__Image"></span>
-            <span className="Products__CardTitle">Lorem ipsum dolor sit amet</span>
-            <span className="Products__CardDescription">men's clothing</span>
-            <div className="Products__PriceBox">
-              <span className="Products__Price">$49.97</span>
-              <span className="Products__Price Discounted">$99.95</span>
-            </div>
-          </div>
-          <div className="Products__Card">
-            <span className="Products__Image"></span>
-            <span className="Products__CardTitle">Lorem ipsum dolor sit amet</span>
-            <span className="Products__CardDescription">men's clothing</span>
-            <div className="Products__PriceBox">
-              <span className="Products__Price">$49.97</span>
-              <span className="Products__Price Discounted">$99.95</span>
-            </div>
-          </div>
+          {
+            products != null &&
+            products.map(x =>
+              <div key={x.id} className="Products__Card">
+                <span className="Products__Image"></span>
+                <span className="Products__CardTitle">{x.title}</span>
+                <span className="Products__CardDescription">men's clothing</span>
+                <div className="Products__PriceBox">
+                  <span className="Products__Price">$49.97</span>
+                  <span className="Products__Price Discounted">$99.95</span>
+                </div>
+              </div>
+            )
+          }
         </div>
       </div>
     </div>
