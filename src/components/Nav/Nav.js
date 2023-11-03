767fc97fbb5c159ea24389302db3ebd820b1bfd8
SirBruno <bruno.webjr@gmail.com>
2023-11-03 14:51:28 +0900

    added navigation.

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -2,9 +2,20 @@ import React from 'react'
 import './Nav.css'
 
 function Nav() {
-  return(
+  return (
     <nav className="nav">
-      <span className="nav__text">Everything 50% off.</span>
+      <span className="nav__text">Work in progress</span>
+      <div className="nav__menu container">
+        <div className="nav__logo">Cupcake</div>
+        <div className="nav__links">
+          <a href="#.com">Clothes</a>
+          <a href="#.com">Jewelery</a>
+        </div>
+      </div>
+      <span className="nav__text2">
+        <span>⏱️Everything 50% off until {new Date((new Date()).getTime() + (60 * 60 * 24 * 1000)).toLocaleDateString('en-US', { day: 'numeric', month: 'numeric' })}</span>
+        <span className="nav__text2_sub">The more you buy, the more you save!</span>
+      </span>
     </nav>
   )
 }
