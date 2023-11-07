e5808f14849b8e44afabb80c51f1c51abf256e8e
SirBruno <bruno.webjr@gmail.com>
2023-11-07 16:36:42 +0900

    added category divisions to the homepage.

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -26,25 +26,49 @@ function Product() {
   console.log("products")
   console.log(products)
 
+  const filteredProducts = (category) => {
+    return products != null && products.filter(product => product.category === category).map(x =>
+      <div key={x.id} className="Products__Card">
+        <img alt={x.title} src={x.image} className="Products__Image"></img>
+        <span className="Products__CardTitle">{x.title}</span>
+        <span className="Products__CardDescription">{x.category}</span>
+        <div className="Products__PriceBox">
+          <span className="Products__Price">{`$` + (x.price / 2).toFixed(2)}</span>
+          <span className="Products__Price Discounted">{`$` + x.price}</span>
+        </div>
+      </div>
+    )
+  }
+
+  console.log("filtered products:")
+  if (products != null) {
+    console.log(filteredProducts("men's clothing"))
+  }
+
   return (
     <div className="Products">
       <div className="Products__Container">
-        <h3>Products</h3>
+        <h3>Men's clothing</h3>
         <div className="Products__Grid">
-          {
-            products != null &&
-            products.map(x =>
-              <div key={x.id} className="Products__Card">
-                <img alt={x.title} src={x.image} className="Products__Image"></img>
-                <span className="Products__CardTitle">{x.title}</span>
-                <span className="Products__CardDescription">{x.category}</span>
-                <div className="Products__PriceBox">
-                  <span className="Products__Price">{`$` + (x.price/2).toFixed(2)}</span>
-                  <span className="Products__Price Discounted">{`$` + x.price}</span>
-                </div>
-              </div>
-            )
-          }
+          { filteredProducts("men's clothing") }
+        </div>
+      </div>
+      <div className="Products__Container">
+        <h3>Women's clothing</h3>
+        <div className="Products__Grid">
+          { filteredProducts("women's clothing") }
+        </div>
+      </div>
+      <div className="Products__Container">
+        <h3>Jewelery</h3>
+        <div className="Products__Grid">
+          { filteredProducts("jewelery") }
+        </div>
+      </div>
+      <div className="Products__Container">
+        <h3>Electronics</h3>
+        <div className="Products__Grid">
+          { filteredProducts("electronics") }
         </div>
       </div>
     </div>
