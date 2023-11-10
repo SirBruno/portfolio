8c67bee859c468a86e7ec1589eaba419822b78c5
SirBruno <bruno.webjr@gmail.com>
2023-11-10 16:46:42 +0900

    added mobile menu page

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -5,6 +5,7 @@ import './Nav.css'
 function Nav() {
 
   const [PRs, SetPRs] = useState(null);
+  const [mobileMenu, SetMobileMenu] = useState(true);
 
   useEffect(() => {
     async function fetchData() {
@@ -22,20 +23,33 @@ function Nav() {
     fetchData()
   }, []);
 
+  const MobileMenuClick = () => {
+    if (mobileMenu) {
+      document.querySelector(".mobileMenu_container").style.display = "flex"
+      document.querySelector(".mobileMenu").classList.add("mobileMenu_Active")
+    } else {
+      document.querySelector(".mobileMenu_container").style.display = "none"
+      document.querySelector(".mobileMenu").classList.remove("mobileMenu_Active")
+    }
+
+    SetMobileMenu(!mobileMenu)
+  }
+
   return (
     <nav className="nav">
       <span className="nav__text">WIP / Latest update ({PRs != null && PRs[0].closed_at.replace(/(\d{4})-(\d{2})-(\d{2}).*/, '$2/$3/$1')
- }):&nbsp;<a href={PRs != null && PRs[0]._links.html.href} target="_blank" rel="noreferrer">{PRs != null && PRs[0].title}</a></span>
+      }):&nbsp;<a href={PRs != null && PRs[0]._links.html.href} target="_blank" rel="noreferrer">{PRs != null && PRs[0].title}</a></span>
       <div className="nav__menu container">
         <div className="nav__logo">Cupcake</div>
         <div className="nav__links">
           <a href="#.com">Clothes</a>
           <a href="#.com">Jewelery</a>
         </div>
-        <div className="mobileMenu">
+        <div onClick={() => MobileMenuClick()} className="mobileMenu">
           <div className="mobileMenu_bar1"></div>
           <div className="mobileMenu_bar2"></div>
         </div>
+        <div className="mobileMenu_container">stuff</div>
       </div>
       <span className="nav__text2">
         <span>⏱️Everything 50% off until {new Date((new Date()).getTime() + (60 * 60 * 24 * 1000)).toLocaleDateString('en-US', { day: 'numeric', month: 'numeric' })}</span>
