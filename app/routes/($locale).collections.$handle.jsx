16f5c3711a359540f1ce72d05ff1e8bfdffebf2a
SirBruno <bruno.webjr@gmail.com>
2024-04-15 16:33:06 +0900

    added new header, hero, cta section

--- app/routes/($locale).collections.$handle.jsx
+++ app/routes/($locale).collections.$handle.jsx
@@ -1,122 +1,58 @@
-import {json, redirect} from '@shopify/remix-oxygen';
-import {useLoaderData, Link} from '@remix-run/react';
-import {
-  Pagination,
-  getPaginationVariables,
-  Image,
-  Money,
-} from '@shopify/hydrogen';
-import {useVariantUrl} from '~/lib/variants';
+import { json } from '@shopify/remix-oxygen'
+import { useLoaderData, Link } from '@remix-run/react'
+import { Pagination, getPaginationVariables, Image, Money, } from '@shopify/hydrogen'
+import { useVariantUrl } from '~/lib/variants'
 
-/**
- * @type {MetaFunction<typeof loader>}
- */
-export const meta = ({data}) => {
-  return [{title: `Hydrogen | ${data?.collection.title ?? ''} Collection`}];
-};
+/** @type {MetaFunction<typeof loader>} */
+export const meta = ({ data }) => { return [{ title: `Hydrogen | ${data?.collection.title ?? ''} Collection` }] }
 
-/**
- * @param {LoaderFunctionArgs}
- */
-export async function loader({request, params, context}) {
-  const {handle} = params;
-  const {storefront} = context;
-  const paginationVariables = getPaginationVariables(request, {
-    pageBy: 8,
-  });
+/** @param {LoaderFunctionArgs} */
+export async function loader({ request, context }) {
+  const { storefront } = context
+  const paginationVariables = getPaginationVariables(request, { pageBy: 8, })
 
-  if (!handle) {
-    return redirect('/collections');
-  }
+  const { collection } = await storefront.query(COLLECTION_QUERY, { variables: { handle: "men", ...paginationVariables }, })
 
-  const {collection} = await storefront.query(COLLECTION_QUERY, {
-    variables: {handle, ...paginationVariables},
-  });
-
-  if (!collection) {
-    throw new Response(`Collection ${handle} not found`, {
-      status: 404,
-    });
-  }
-  return json({collection});
+  return json({ collection })
 }
 
 export default function Collection() {
   /** @type {LoaderReturnData} */
-  const {collection} = useLoaderData();
+  const { collection } = useLoaderData()
 
   return (
     <div className="collection">
-      <h1>{collection.title}</h1>
-      <p className="collection-description">{collection.description}</p>
       <Pagination connection={collection.products}>
-        {({nodes, isLoading, PreviousLink, NextLink}) => (
-          <>
-            <PreviousLink>
-              {isLoading ? 'Loading...' : <span>↑ Load previous</span>}
-            </PreviousLink>
-            <ProductsGrid products={nodes} />
-            <br />
-            <NextLink>
-              {isLoading ? 'Loading...' : <span>Load more ↓</span>}
-            </NextLink>
-          </>
-        )}
+        {({ nodes }) => (<ProductsGrid products={nodes} />)}
       </Pagination>
     </div>
-  );
+  )
 }
 
-/**
- * @param {{products: ProductItemFragment[]}}
- */
-function ProductsGrid({products}) {
+/** @param {{products: ProductItemFragment[]}} */
+function ProductsGrid({ products }) {
   return (
     <div className="products-grid">
-      {products.map((product, index) => {
-        return (
-          <ProductItem
-            key={product.id}
-            product={product}
-            loading={index < 8 ? 'eager' : undefined}
-          />
-        );
-      })}
+      {products.map((product, index) => { return (<ProductItem key={product.id} product={product} loading={index < 8 ? 'eager' : undefined} />) })}
     </div>
-  );
+  )
 }
 
-/**
- * @param {{
- *   product: ProductItemFragment;
- *   loading?: 'eager' | 'lazy';
- * }}
- */
-function ProductItem({product, loading}) {
-  const variant = product.variants.nodes[0];
-  const variantUrl = useVariantUrl(product.handle, variant.selectedOptions);
+/** @param {{ product: ProductItemFragment; loading?: 'eager' | 'lazy'; }} */
+function ProductItem({ product, loading }) {
+  const variant = product.variants.nodes[0]
+  const variantUrl = useVariantUrl(product.handle, variant.selectedOptions)
   return (
-    <Link
-      className="product-item"
-      key={product.id}
-      prefetch="intent"
-      to={variantUrl}
-    >
+    <Link className="product-item" key={product.id} prefetch="intent" to={variantUrl} >
       {product.featuredImage && (
-        <Image
-          alt={product.featuredImage.altText || product.title}
-          aspectRatio="1/1"
-          data={product.featuredImage}
-          loading={loading}
-          sizes="(min-width: 45em) 400px, 100vw"
-        />
+        <Image alt={product.featuredImage.altText || product.title} aspectRatio="1/1" data={product.featuredImage} loading={loading} sizes="(min-width: 45em) 400px, 100vw" />
       )}
       <h4>{product.title}</h4>
       <small>
         <Money data={product.priceRange.minVariantPrice} />
       </small>
     </Link>
-  );
+  )
 }
 
 const PRODUCT_ITEM_FRAGMENT = `#graphql
@@ -152,9 +88,8 @@ const PRODUCT_ITEM_FRAGMENT = `#graphql
       }
     }
   }
-`;
+`
 
-// NOTE: https://shopify.dev/docs/api/storefront/2022-04/objects/collection
 const COLLECTION_QUERY = `#graphql
   ${PRODUCT_ITEM_FRAGMENT}
   query Collection(
@@ -189,7 +124,7 @@ const COLLECTION_QUERY = `#graphql
       }
     }
   }
-`;
+`
 
 /** @typedef {import('@shopify/remix-oxygen').LoaderFunctionArgs} LoaderFunctionArgs */
 /** @template T @typedef {import('@remix-run/react').MetaFunction<T>} MetaFunction */
